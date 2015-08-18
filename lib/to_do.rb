fd5a2997f603747d13f233ab301ccb58409f0335
Brandon Seroyer & Kellen Kolbeck <student@epicodus.com>
2015-08-18 14:02:26 -0700

    add spec in code the database and list ID

--- lib/to_do.rb
+++ lib/to_do.rb
@@ -7,6 +7,17 @@ class Task
   end
 
   define_singleton_method(:all) do
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
     tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch('description')
+      tasks.push(Task.new({:description => description}))
+    end
+    tasks
   end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}')")
+  end
+
 end
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,5 +1,14 @@
 require ('rspec')
 require ('to_do')
+require('pg')
+
+DB = PG.connect({:dbname => 'to_do_list_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+end
 
 describe(Task) do
   describe("#description") do
@@ -13,13 +22,20 @@ describe(Task) do
     it("saves description for a task") do
       test_description = Task.new({:description => "feed the lion"})
       test_description.save()
-      expect(Tasks.all()).to(eq([test_description]))
+      expect(Task.all()).to(eq([test_description]))
     end
   end
 
   describe(".all") do
     it("creates an array to save in") do
     expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe("list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn sql", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
     end
   end
 
