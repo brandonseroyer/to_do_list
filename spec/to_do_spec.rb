4662d50936ef5de00d1e7199fcc785f5da17a580
Brandon Seroyer & Kellen Kolbeck <student@epicodus.com>
2015-08-18 15:04:13 -0700

    Add spec and working code to determine strings as equals.

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -13,14 +13,14 @@ end
 describe(Task) do
   describe("#description") do
     it("gives the description of the task") do
-      test_description = Task.new({:description => "scrub the zebra"})
+      test_description = Task.new({:description => "scrub the zebra", :list_id => 1})
       expect(test_description.description()).to(eq("scrub the zebra"))
     end
   end
 
   describe("#save") do
     it("saves description for a task") do
-      test_description = Task.new({:description => "feed the lion"})
+      test_description = Task.new({:description => "feed the lion", :list_id => 1})
       test_description.save()
       expect(Task.all()).to(eq([test_description]))
     end
@@ -32,11 +32,18 @@ describe(Task) do
     end
   end
 
-  describe("list_id") do
+  describe("#list_id") do
     it("lets you read the list ID out") do
       test_task = Task.new({:description => "learn sql", :list_id => 1})
       expect(test_task.list_id()).to(eq(1))
     end
   end
 
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(task1).to(eq(task2))
+    end
+  end
 end
